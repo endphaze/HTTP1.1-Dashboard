83777c2ed35d763e7d3c2ee303e7b8ca6e486275
icenshy <icenshy@gmail.com>
2026-03-29 16:19:52 +0700

    edit expose data

--- main.py
+++ main.py
@@ -153,4 +153,4 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8081)
+    uvicorn.run(app, host="0.0.0.0", port=8081)
